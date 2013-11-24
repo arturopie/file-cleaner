928a3a555f14510191640d9bfb0b23208718d21e
Arturo Pie <arturotd08@yahoo.ca>
2013-11-24 00:53:01 -0500

    remove lines that match a pattern

--- lib/file_cleaner.rb
+++ lib/file_cleaner.rb
@@ -1,5 +1,23 @@
 require "file_cleaner/version"
 
 module FileCleaner
-  # Your code goes here...
+  def self.remove args
+    file_path = args[:file_path]
+    pattern = args[:pattern]
+
+    lines = []
+    File.open(file_path, 'r') do |f|
+      lines = f.readlines
+    end
+
+    result = lines.reject do |line|
+      line =~ pattern
+    end
+
+    File.open(file_path, 'w+') do |f|
+      result.each do |line|
+        f.write line
+      end
+    end
+  end
 end
---
+++ spec/file_cleaner_spec.rb
@@ -0,0 +1,34 @@
+require 'rspec'
+require 'spec_helper'
+require 'file_cleaner'
+
+describe FileCleaner do
+
+  describe '.remove' do
+    before :all do
+      @file_path = 'spec/tmp/a_file'
+      File.open(@file_path, 'w+') do |f|
+        f.write "line 1\n"
+        f.write "Remove me\n"
+        f.write "line 2\n"
+        f.write "line 3\n"
+      end
+    end
+
+    context 'removing using pattern without passing a range' do
+      before do
+        FileCleaner.remove(pattern: /Remove me/,
+                           file_path: @file_path)
+      end
+      subject { result }
+      it { should eq(["line 1\n", "line 2\n", "line 3\n"]) }
+    end
+
+    def result
+      f = File.open(@file_path, 'r')
+      lines = f.readlines
+      f.close
+      lines
+    end
+  end
+end
